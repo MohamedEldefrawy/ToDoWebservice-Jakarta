44f974bb510bca19148896c000e71447e3bfa074
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-14 23:35:49 +0200

    Add JDBC dependcy and create DbContext

--- src/main/java/com/todo/data/DbContext.java
+++ src/main/java/com/todo/data/DbContext.java
@@ -1,4 +1,24 @@
 package com.todo.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.todo.config.DataBaseConfiguration.*;
+
 public class DbContext {
+    public static Connection openDbConnection() {
+        Connection con;
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            con = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
 }
